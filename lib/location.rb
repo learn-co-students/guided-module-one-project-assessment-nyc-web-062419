0141fdaafefcda186c59cb3bd463b30083a411a2
adriennem76 <adrienne.c.miranda@gmail.com>
2019-07-09 11:03:23 -0400

    merge conflicts

--- lib/location.rb
+++ lib/location.rb
@@ -1,8 +1,4 @@
 class Location < ActiveRecord::Base
     has_many :sightings
     has_many :users, through: :sightings
-<<<<<<< HEAD
-    
-=======
->>>>>>> 89686b73f43bff98fd8bcb974509c234a54713c5
 end
