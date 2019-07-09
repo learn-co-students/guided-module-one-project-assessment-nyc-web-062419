9e0d22360112f0c1113517244290fb86af8849c4
adriennem76 <adrienne.c.miranda@gmail.com>
2019-07-09 14:22:34 -0400

    before seeding db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,22 +1,34 @@
 require 'pry'
 require 'json'
 require 'date'
+require_relative '../config/environment.rb'
+
+User.delete_all
+Location.delete_all
+Sighting.delete_all
+
 ufo_array = File.read('./lib/ufo-sightings.json')
 ufo_hash = JSON.parse(ufo_array)
+
+i = 500
+while i > 0
+    user = User.create(name: Faker::Name.name)
+    i -= 1
+end
 
 us_ufos = ufo_hash.select {|hash| hash["country"] == "us"}
 
 us_ufos.each do |hash|
-    # location = Location.create(city: hash["city"], state: hash["state"])
+    
+    location = Location.find_or_create_by(city: hash["city"], state: hash["state"])
+    
+    date_array = hash["datetime"].tr("/", ":").split(/[: ]/)
+    date_array = date_array.collect{|num| num.to_i}
+    date = DateTime.new(date_array[2], date_array[0], date_array[1], date_array[3], date_array[4])
 
-    #[[10,10,1949], [20,30]]
-    binding.pry
-    date_array = hash["datetime"].split(" ")
-    date_array= date_array.split(/[/:] /) 
-    binding.pry
-    date = DateTime.new(date_array[0][2], date_array[0][0], date_array[0][1], date_array[1][0], date_array[1][1])
-    binding.pry
-    # sighting = Sighting.create(user_id: ,location_id: location.id, date: date, shape: hash["shape"])
+    user = rand(1..User.all.length)
+    sighting = Sighting.create(user_id: user,location_id: location.id, date: date, shape: hash["shape"])
+    
 end
 
 # binding.pry
