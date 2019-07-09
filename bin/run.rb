417a89411b7bd82adac5b120d786460b30cdeb26
adriennem76 <adrienne.c.miranda@gmail.com>
2019-07-09 17:09:04 -0400

    before resetting users

--- bin/run.rb
+++ bin/run.rb
@@ -1,3 +1,9 @@
 require_relative '../config/environment'
 
-puts "hello world"
+cli = CommandLineInterface.new
+cli.greet
+user = cli.get_user
+cli.get_sighting_by_location
+
+
+# puts "hello world"
---
+++ lib/cli.rb
@@ -0,0 +1,35 @@
+require 'pry'
+class CommandLineInterface
+    
+    def greet
+        puts 'Greetings, Earthling.'
+    end
+
+    def get_user
+        puts "Enter your username:"
+        name = gets.chomp
+        user = User.find_or_create_by(name: name)
+        puts "Welcome #{name}."
+        user
+    end
+
+    def get_sighting_by_location
+        puts "Please enter a city and state to find sightings in that city:"
+        puts "City:"
+        city = gets.chomp.downcase
+        puts "State(XX):"
+        state = gets.chomp.downcase
+        location_array = Location.select {|location| location.city.include?(city) && location.state == state}
+        
+        sightings = location_array.collect do |location|
+           
+            Sighting.select {|sighting| sighting.location_id == location.id}
+        end.flatten
+       
+        sightings.each_with_index do |sighting, index|
+            binding.pry
+            puts sightings[index]
+        end
+    end
+
+end
