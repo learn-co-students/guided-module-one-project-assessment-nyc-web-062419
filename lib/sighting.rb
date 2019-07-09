07d17f9efeec6c82ed9126d46750f86643ae75ee
adriennem76 <adrienne.c.miranda@gmail.com>
2019-07-09 11:04:56 -0400

    end merge conflicts

--- lib/sighting.rb
+++ lib/sighting.rb
@@ -1,10 +1,4 @@
 class Sighting < ActiveRecord::Base
-    belongs_to :users
-<<<<<<< HEAD
-    belongs_to :locations
-    
-end
-=======
+    belongs_to :user
     belongs_to :location
 end
->>>>>>> 89686b73f43bff98fd8bcb974509c234a54713c5
