b4145efae506ecb20141deec0aab99a3c0eed465
adriennem76 <adrienne.c.miranda@gmail.com>
2019-07-10 15:20:11 -0400

    fixed bugs

--- bin/run.rb
+++ bin/run.rb
@@ -3,11 +3,23 @@ require_relative '../config/environment'
 cli = CommandLineInterface.new
 cli.greet
 user = cli.get_user
+cli.help
+puts "Enter a command from the list above:"
+command = gets.chomp.downcase
+if command == "report"
+    cli.create_a_sighting(user)
+elsif command == "find"
+    cli.get_sighting_by_location
+elsif command == "edit"
+    cli.edit_sighting(user)
+elsif command == "delete"
+    cli.delete_sighting(user)
+elsif command == "my sightings"
+    cli.my_sightings(user)
+elsif command == "help"
+    cli.help
+end
 
 
-# cli.get_sighting_by_location
-# cli.create_a_sighting(user)
-# user = cli.get_user
-cli.my_sightings(user)
 
 # puts "hello world"
--- lib/cli.rb
+++ lib/cli.rb
@@ -1,6 +1,6 @@
 require 'pry'
 class CommandLineInterface
-    
+    include Kernel
     def greet
         puts 'Greetings, Earthling.'
     end
@@ -26,18 +26,21 @@ class CommandLineInterface
             Sighting.select {|sighting| sighting.location_id == location.id}
         end.flatten
        
-        sightings.each do |sighting|
-            
-            puts "
-            User: #{User.find(sighting.user_id).name},
-            City: #{Location.find(sighting.location_id).city},
-            State: #{Location.find(sighting.location_id).state},
-            Date: #{sighting.date},
-            Shape: #{sighting.shape}"
+        sightings.each do |sighting| 
+            put_sighting(sighting)
         end
     end
 
-    def create_a_sighting(user)
+    def put_sighting(sighting)
+        puts "
+        User: #{User.find(sighting.user_id).name},
+        City: #{Location.find(sighting.location_id).city},
+        State: #{Location.find(sighting.location_id).state},
+        Date: #{sighting.date},
+        Shape: #{sighting.shape}"
+    end
+
+    def get_a_location
         puts "Please enter the city and state where your sighting occurred."
         puts "City:"
         city = gets.chomp.downcase
@@ -46,21 +49,31 @@ class CommandLineInterface
 
         cities = Location.select {|location| location.city.include?(city) && location.state == state}
         cities << Location.select {|location| city.include?(location.city) && location.state == state}
-        cities = cities.flatten
+        cities = cities.flatten.uniq
+        
         if cities != []
-            city_list = cities.collect {|location| location.city}
-            puts "Did you mean one of the following cities in #{state}?"
-            puts city_list
-            puts "Please enter one of the above city names or re-enter your city name:"
-            puts "City:"
-            city = gets.chomp.downcase
+            if Location.find_by(city: city, state: state) == nil 
+                city_list = cities.collect {|location| location.city}
+                puts "Did you mean one of the following cities in #{state}?"
+                puts city_list
+                puts "Please enter one of the above city names or re-enter your city name:"
+                puts "City:"
+                city = gets.chomp.downcase
+            end
         end
 
         location = Location.find_or_create_by(city: city, state: state)
-        
+    end
+
+    def get_a_shape
         puts "Enter a one-word description of the shape of the ufo spotted (i.e. cylinder, circle, sphere, disk, light, fireball, triangle, cross, crescent, etc.):"
         shape = gets.chomp
-        
+    end
+
+    def create_a_sighting(user)
+        location = get_a_location
+        shape = get_a_shape
+          
         date = DateTime.now
 
         sighting = Sighting.create do |s|
@@ -72,12 +85,7 @@ class CommandLineInterface
      
         puts "Your sighting has been created!"
 
-        puts "
-        User: #{User.find(sighting.user_id).name},
-        City: #{Location.find(sighting.location_id).city},
-        State: #{Location.find(sighting.location_id).state},
-        Date: #{sighting.date},
-        Shape: #{sighting.shape}"
+        put_sighting(sighting)
 
     end
 
@@ -86,15 +94,75 @@ class CommandLineInterface
         if user.sightings == []
             puts "You do not have any sightings yet!"
         else
+            i=1
             user.sightings.each do |sighting|
+
                 puts "
-                User: #{user.name},
-                City: #{Location.find(sighting.location_id).city},
-                State: #{Location.find(sighting.location_id).state},
-                Date: #{sighting.date},
-                Shape: #{sighting.shape}"
+                Sighting: #{i}"
+                put_sighting(sighting)
+                i+=1
             end
         end
+    end
+
+    def edit_sighting(user)
+        if my_sightings(user)
+            puts 'Select which sighting you would like to edit by entering its sighting number:'
+            sighting = nil
+            while sighting = nil
+                num = gets.chomp.to_i
+               
+                sighting = user.sightings[num-1]
+                if sighting
+                    break
+                end
+                puts "Please enter a valid sighting number."
+                
+            end
+            put_sighting(sighting)
+
+            puts 'Would you like to edit the shape (y/n)?'
+            answer = gets.chomp
+           
+            if answer.downcase == ('y' || 'yes')
+                sighting.shape = get_a_shape
+                sighting.save
+                put_sighting(sighting)
+                sighting
+            end
+            puts 'Would you like to edit the location (y/n)?'
+            answer = gets.chomp
+            if answer.downcase == ('y' || 'yes')
+                sighting.location_id = get_a_location.id
+                sighting.save
+                put_sighting(sighting)
+            end
+        end
+
+    end
+
+    def delete_sighting(user)
+        my_sightings(user)
+        puts 'Select which sighting you would like to delete by entering its sighting number:'
+        num = gets.chomp.to_i
+        sighting = user.sightings[num-1]
+        put_sighting(sighting)
+        puts 'Are you sure you want to delete this sighting (y/n)?'
+        answer = gets.chomp
+        if answer.downcase == ('y' || 'yes')
+            sighting.delete
+            puts "Your sighting has been deleted. The government will never find out."
+        end
+    end
+
+    def help
+        puts "
+        report : report a UFO sighting,
+        find : search for sightings by location,
+        my sightings : displays your UFO sightings,
+        edit : edit one of your UFO sightings,
+        delete : delete one of your UFO sightings,
+        help : displays this list"
     end
 
 end
